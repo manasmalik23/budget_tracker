ea82c0aa05c2500d439bc8dfe31efacf51c5f87b
Manas Malik <manas.malik@ihx.in>
2022-05-27 10:46:27 +0530

    changing reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -12,16 +12,22 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_INCOME:
             state.ledger.income += action.ledgerEntry.amount
-            state.ledgerEntry.push(action.ledgerEntry)
-            return state
+            // state.ledgerEntry[state.ledgerEntry.length] = action.ledgerEntry
+
+            // state.ledgerEntry.push(action.ledgerEntry)
+            return {
+                ...state, ledgerEntry: [state.ledgerEntry, action.ledgerEntry]
+            };
         case actionTypes.ADD_EXPENSE:
             state.ledger.expense += action.ledgerEntry.amount
-            state.ledgerEntry.push(action.ledgerEntry)
-            return state
-        case actionTypes.UPDATE_TRANSACTION:
-            state.ledger.filter((action) => action.ledgerEntry.id['id'] !== state.ledger.id);
-            return state
-
+            // state.ledgerEntry[state.ledgerEntry.length] = action.ledgerEntry
+            // state.ledgerEntry.push(action.ledgerEntry)
+            return {
+                ...state, ledgerEntry: [state.ledgerEntry, action.ledgerEntry]
+            };
+        // case actionTypes.UPDATE_TRANSACTION:
+        //     state.ledger.filter((action) => action.ledgerEntry.id['id'] !== state.ledger.id);
+        //     return state
     }
     return state;
 };
